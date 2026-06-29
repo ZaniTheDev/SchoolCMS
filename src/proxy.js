efbb55589382d46e9d743783ec0f1cde2aa232f2
Hamzah <hamzahtizani@gmail.com>
2026-06-29 20:10:15 +0700

    fix?

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
+export async function proxy(req) {
   const { pathname } = req.nextUrl;
 
   const token = await getToken({
